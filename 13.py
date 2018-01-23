0e3406f8652588c7a4bd4aa4cca9d475511c4c25
JP Zhang <chooperchang@gmail.com>
2018-01-23 14:33:54 +0000

    add a13

---
+++ 13.py
@@ -0,0 +1,15 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+with open(sys.argv[1], "r", encoding="utf-8") as f1, open(sys.argv[2], "r", encoding="utf-8") as f2:
+    line1, line2 = f1.readlines(), f2.readlines()
+
+with open("merge.txt", "w", encoding="utf-8") as write:
+     write.write(
+        "\n".join(
+            ["\t".join([col1.rstrip(), col2.rstrip()])
+              for col1, col2 in zip(line1, line2)]
+         )
+    )
