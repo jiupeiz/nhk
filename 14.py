ab57ddc4cf068d666eb6f896f32f96f218f40121
JP Zhang <chooperchang@gmail.com>
2018-01-23 14:52:58 +0000

    add a14

---
+++ 14.py
@@ -0,0 +1,10 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+with open(sys.argv[1], "r", encoding="utf-8") as f:
+    lines = f.readlines()
+
+for line in lines[:int(sys.argv[2])]:
+    print(line.rstrip())
