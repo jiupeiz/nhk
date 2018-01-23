e852b911aa83b4261673000bd7577f9241076d57
JP Zhang <chooperchang@gmail.com>
2018-01-23 14:53:54 +0000

    changed a13

--- 13.py
+++ 13.py
@@ -7,9 +7,5 @@ with open(sys.argv[1], "r", encoding="utf-8") as f1, open(sys.argv[2], "r", enco
     line1, line2 = f1.readlines(), f2.readlines()
 
 with open("merge.txt", "w", encoding="utf-8") as write:
-     write.write(
-        "\n".join(
-            ["\t".join([col1.rstrip(), col2.rstrip()])
-              for col1, col2 in zip(line1, line2)]
-         )
-    )
+     write.writelines("\n".join(["\t".join([col1.rstrip(), col2.rstrip()]) \
+                                 for col1, col2 in zip(line1, line2)]))
