39fd3475d4624d51eb57f90d2c35b8e1d3f29055
JP Zhang <chooperchang@gmail.com>
2018-01-24 18:10:27 +0000

    add a21 & remove __pycache__

---
+++ 21.py
@@ -0,0 +1,10 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+from gct import get_country_text
+
+lines = get_country_text("jawiki-country.json", u'イギリス').split("\n")
+
+for line in lines:
+    if "Category" in line:
+        print(line)
