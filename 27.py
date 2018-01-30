b20f58ebd31bcd519c47db406d700dfbe3e2388a
JP Zhang <chooperchang@gmail.com>
2018-01-30 14:13:00 +0000

    add a27

---
+++ 27.py
@@ -0,0 +1,14 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+from gct import get_country_text
+from gtv import get_template_value
+import re
+
+country_text = get_country_text("jawiki-country.json", u"イギリス")
+tps_val = get_template_value(country_text)
+
+for k, v in tps_val.items():
+    plant_val = re.sub(r"'{2,5}", r"", v)
+    plant_val = re.sub(r"\[{2}(.*?)\]{2}", r"\1", plant_val)
+    print(k + "\t" + plant_val)
