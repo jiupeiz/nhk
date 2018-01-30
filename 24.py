55e513cdc86f7e963a0699053698199983305ea4
JP Zhang <chooperchang@gmail.com>
2018-01-30 13:43:49 +0000

    modified a24's regex

--- 24.py
+++ 24.py
@@ -7,6 +7,6 @@ from gct import get_country_text
 lines = get_country_text("jawiki-country.json", u"イギリス").split("\n")
 
 for line in lines:
-    file_line = re.search(r"(File|ファイル)\:(.*?\.\w\w\w\w?)\|", line)
+    file_line = re.search(r"(File|ファイル)\:(.*?\.\w{3,4})\|", line)
     if file_line is not None:
         print(file_line.group(2))
