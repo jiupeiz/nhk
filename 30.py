4bbaa0bb09bb4fc2587b3e937cf53a2dba2507ad
JP Zhang <chooperchang@gmail.com>
2018-02-02 13:50:42 +0000

    add a30 & moduled a30

---
+++ 30.py
@@ -0,0 +1,21 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+
+def get_morphs_info(filename):
+    docu = list()
+    sent = list()
+    for line in open(filename).readlines():
+        if line.startswith("EOS"):
+            docu.append(sent)
+            sent = list()
+            continue
+        word, info = line.rstrip().split("\t")
+        info_list = info.split(",")
+        morphs = {"word": word, "base": info_list[6], "pos": info_list[0], "pos1": info_list[1]}
+        sent.append(morphs)
+    return docu
+
+
+if __name__ == '__main__':
+    text = get_morphs_info("./neko.txt.mecab")
